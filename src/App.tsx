09e1e54b1704c259ada4d279cedd861072073d2a
Sharad072 <sharad.gumbar@capx.global>
2024-05-11 12:32:32 +0530

    changes for todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Button from './components/Button';
+import TodoRow from './components/TodoRow';
+import "./App.css"
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<{ id: number, label: string, completed: boolean }[]>([]);
+  const [inputText, setInputText] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() !== '') {
+      const newTodo = {
+        id: todos.length + 1,
+        label: inputText,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setInputText('');
+    }
+  };
+
+  const handleToggleTodo = (id: number) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='todo'>
+      <h1 className='header'>Todo</h1>
+      <div className="todo-list">
+        {todos.map(todo => (
+          <TodoRow
+            key={todo.id}
+            label={todo.label}
+            checked={todo.completed}
+            onCheckboxChange={() => handleToggleTodo(todo.id)}
+            onDeleteButtonClick={() => handleDeleteTodo(todo.id)}
+          />
+        ))}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Add task here..."
+        />
+        <Button onClick={handleAddTodo} variant="big">Add</Button>
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
